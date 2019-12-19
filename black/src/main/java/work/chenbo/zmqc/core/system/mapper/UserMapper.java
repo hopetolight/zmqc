a5411699e1fb9cf832db86f6d482db987ad82e60
ChenBo <hopetolight@outlook.com>
2019-12-19 19:25:13 +0800

    [mod] 调整项目结构 [add] 添加front

---
+++ black/src/main/java/work/chenbo/zmqc/core/system/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package work.chenbo.zmqc.core.system.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import work.chenbo.zmqc.core.system.model.User;
+
+/**
+ * @author ChenBo
+ * @className UserMapper
+ * @date 2019/12/18
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
---
+++ black/src/main/java/work/chenbo/zmqc/core/system/model/UserRole.java
@@ -0,0 +1,33 @@
+package work.chenbo.zmqc.core.system.model;
+
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import org.springframework.data.annotation.Transient;
+
+
+/**
+ * @author ChenBo
+ * @className UserRole
+ * @date 2019/12/17
+ */
+@Data
+@TableName("t_user_role")
+@ApiModel(value = "用户角色")
+public class UserRole {
+
+    private static final long serialVersionUID = 1L;
+
+    @ApiModelProperty(value = "用户唯一id")
+    private String userId;
+
+    @ApiModelProperty(value = "角色唯一id")
+    private String roleId;
+
+    @Transient
+    @TableField(exist=false)
+    @ApiModelProperty(value = "角色名")
+    private String roleName;
+}
